255ad983ec656aa000678543f537a59cfd2dbc46
SnakyDappy <xgamer200000000@gmail.com>
2025-02-17 19:20:31 +0100

    filmare

--- src/components/Poza.js
+++ src/components/Poza.js
@@ -4,13 +4,13 @@ import ReactPlayer from 'react-player';
 function Poza() {
   return (
     <div className="center-containervideo">
-       <ReactPlayer 
-      url="https://www.youtube.com/watch?v=B8oiiB0fcpU&ab_channel=SnakyDappy"
-      controls
-    />
+      <ReactPlayer 
+        url="https://www.youtube.com/watch?v=B8oiiB0fcpU&ab_channel=SnakyDappy"
+        controls
+        className="responsive-video"
+      />
     </div>
   );
 }
 
 export default Poza;
-
