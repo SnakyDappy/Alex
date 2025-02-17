4bf7e7445676d6bd2d2e7cb097974eaff52fc70f
SnakyDappy <xgamer200000000@gmail.com>
2025-02-17 18:57:22 +0100

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import Photo1 from '../photos/photo (3).jpeg';
 function About() {
   return (
     <section id="about" className="about background" >
-      <img src={Photo1}/>
+      <img src={Photo1} alt="Am fost obligat sa fac descrierea asta"/>
       <div>
         <h2>Despre Noi:</h2>
         <p>ceva ceva ceva</p>
