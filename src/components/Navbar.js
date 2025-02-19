0b98e55eb098c9e186021cb8b66a77c0020d4a93
SnakyDappy <xgamer200000000@gmail.com>
2025-02-19 19:53:01 +0100

    mici

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
     <nav>
       <div className="center-links">
         <a href="#about">About</a>
-        <a href="#gallery">Gallery</a>
+        <a href="#gallery">Reviews</a>
         <a href="#contact">Contact</a>
       </div>
     </nav>
