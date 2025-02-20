193499f04664f0b0c347ecbce32da6d69af18caf
SnakyDappy <xgamer200000000@gmail.com>
2025-02-20 20:19:24 +0100

    ceva

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -4,12 +4,21 @@ import Photo2 from "../photos/photo (3).jpeg";
 import Photo3 from "../photos/photo (4).jpeg";
 import Photo4 from "../photos/photo (5).jpeg";
 import Photo5 from "../photos/photo (6).jpeg";
+import Photo6 from "../photos/vertical (2).jpeg";
+import Photo8 from "../photos/vertical (4).jpeg";
+import Photo9 from "../photos/vertical (5).jpeg";
+import Photo10 from "../photos/vertical (6).jpeg";
+import Photo11 from "../photos/vertical (7).jpeg";
+import Photo12 from "../photos/vertical (8).jpeg";
+import Photo13 from "../photos/vertical (9).jpeg";
 
 function Slider() {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const slides = [Photo1, Photo2, Photo3, Photo4, Photo5];
+  const slides = [
+    Photo1, Photo2, Photo3, Photo4, Photo6, Photo8, Photo9, Photo10, Photo11, Photo12, Photo13, Photo5
+  ];
 
   const nextSlide = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
@@ -37,12 +46,14 @@ function Slider() {
             className = "hidden";
           }
 
+          const isVertical = src.includes("vertical");
+
           return (
             <img
               key={index}
               src={src}
-              alt={`Cocktail Cake Slider ${index + 1}`}
-              className={className}
+              alt={`Slider ${index + 1}`}
+              className={`${className} ${isVertical ? "vertical" : ""}`}
               onClick={() => index === currentIndex && setIsModalOpen(true)}
             />
           );
@@ -55,7 +66,7 @@ function Slider() {
       {isModalOpen && (
         <div className="modal-overlay" onClick={() => setIsModalOpen(false)}>
           <div className="modal-content">
-            <img src={slides[currentIndex]} alt="Enlarged Cocktail Cake" />
+            <img src={slides[currentIndex]} alt="Enlarged Item" />
           </div>
         </div>
       )}
