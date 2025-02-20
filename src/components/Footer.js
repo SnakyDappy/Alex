302ee4f18b9b316e5eab344102d9261b83e75313
SnakyDappy <xgamer200000000@gmail.com>
2025-02-20 16:25:15 +0100

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,33 +1,51 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookSquare, faInstagram, faTiktok, faYoutube, faLinkedin, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope} from '@fortawesome/free-solid-svg-icons';
+
 
 function Footer() {
   return (
     <footer id="contact">
-  <div class="contact-buttons">
+      {/* Email Button */}
+      <div className="contact-buttons">
+        <a
+          href="https://mail.google.com/mail/?view=cm&fs=1&to=namcartela@gmail.com"
+          className="contact-button"
+        >
+          <FontAwesomeIcon icon={faEnvelope} /> Email
+        </a>
 
-    <a href="https://mail.google.com/mail/?view=cm&fs=1&to=namcartela@gmail.com" class="contact-button">
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-        <path d="M12 13.5l-8-6h16l-8 6zm0 1.5l8-6v6l-8 6-8-6v-6l8 6z"></path>
-      </svg>
-      Email
-    </a>
+        <a
+          href="https://wa.me/07namcartela"
+          className="contact-button"
+        >
+          <FontAwesomeIcon icon={faWhatsapp} /> WhatsApp
+        </a>
+      </div>
 
+      
+      <p className="footer-heading">Ne gasesti si pe retelele sociale</p>
+
+
+      {/* Social Media Icons */}
+      <div className="social-icons">
+        <a href="http://" target="_blank" className="social-icon">
+          <FontAwesomeIcon icon={faFacebookSquare} />
+        </a>
+        <a href="http://" target="_blank" className="social-icon">
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
+        <a href="http://" target="_blank" className="social-icon">
+          <FontAwesomeIcon icon={faTiktok} />
+        </a>
+        <a href="http://" target="_blank" className="social-icon">
+          <FontAwesomeIcon icon={faYoutube} />
+        </a>
+      </div>
 
       <p>&copy; 2025 Shot shot events | A snaky project</p>
-
-
-
-    <a href="https://wa.me/07namcartela" class="contact-button">
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-        <path d="M19.9 4.1c-1.7-1.7-4.5-1.7-6.2 0l-1.4 1.4-1.4-1.4c-1.7-1.7-4.5-1.7-6.2 0-1.7 1.7-1.7 4.5 0 6.2l7.8 7.8c.4.4.9.7 1.5.9s1.1-.2 1.5-.6l7.8-7.8c1.7-1.7 1.7-4.5 0-6.2zm-1.3 4.9l-7.8 7.8c-.3.3-.8.5-1.2.6-.4-.1-.8-.3-1.1-.5-.6-.3-1.1-.7-1.7-1.1-.3-.2-.6-.4-.8-.6-.3-.4-.6-.7-.8-1.1-.5-.7-.8-1.5-1.1-2.2-.4-.8-.7-1.7-.9-2.5-.1-.5-.2-.9-.3-1.4-.1-.2-.2-.5-.3-.7-.1-.2-.3-.4-.5-.6l-.2-.2c-.6-.5-1.3-.8-2.1-.8-.7 0-1.4.1-2.1.4-.9.3-1.7.8-2.5 1.6-1.1 1.1-1.6 2.6-1.6 4.1 0 1.6.5 3.1 1.4 4.4l.3.3c.6.5 1.3.9 2.1 1.1s1.6.5 2.4.5c.9 0 1.7-.2 2.5-.7 1.2-.6 2.3-1.5 3.2-2.5l7.8-7.8c1.7-1.7 1.7-4.5 0-6.2z"></path>
-      </svg>
-      WhatsApp
-    </a>
-  </div>
-  
-  
-  </footer>
-
+    </footer>
   );
 }
 
