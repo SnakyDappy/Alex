41faab0405814340f62dee1be9f9d028242820c6
SnakyDappy <51878773+SnakyDappy@users.noreply.github.com>
2025-02-23 13:55:25 +0100

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,11 @@ function About() {
       <img src={Photo1} alt="Am fost obligat sa fac descrierea asta"/>
       <div>
         <h2 className='neonGold'>Despre Noi:</h2>
-        <p className='gradient'>ceva ceva ceva</p>
+        <p className='gradient'>La Shot by Shot Events, ne dedicăm pasiunii de a crea torturi de shoturi alcoolice delicioase și inovative, care aduc o notă de originalitate la fiecare eveniment. Fiecare tort este realizat cu grijă și atenție la detalii, folosind ingrediente de calitate superioară și rețete unice, inspirate din cele mai populare băuturi alcoolice.
+
+Indiferent dacă sărbătoriți o aniversare, o nuntă sau orice altă ocazie specială, torturile noastre de shoturi sunt alegerea perfectă pentru a adăuga un plus de savoare și distracție. Suntem aici pentru a transforma orice eveniment într-o amintire de neuitat, printr-o combinație perfectă de gusturi și prezentare.
+
+Ne mândrim cu dedicarea noastră față de clienți și ne asigurăm că fiecare tort reflectă stilul și dorințele tale. Viziunea noastră este să aducem o experiență gustativă rafinată, care să încânte atât ochii, cât și papilele gustative.</p>
       </div>
     </section>
   );
